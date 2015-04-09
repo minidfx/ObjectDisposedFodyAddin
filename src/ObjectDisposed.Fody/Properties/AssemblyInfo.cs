ee5be079079cbb92b316648bd77cef28f2a7150b
Burgy Benjamin <minidfx@gmail.com>
2015-04-09 22:20:46 +0200

    Add the GlobalAssemblyInfo file and reference it into others projects.

--- src/ObjectDisposed.Fody/Properties/AssemblyInfo.cs
+++ src/ObjectDisposed.Fody/Properties/AssemblyInfo.cs
@@ -1,34 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("ObjectDisposed.Fody")]
-[assembly: AssemblyProduct("ObjectDisposed.Fody")]
-[assembly: AssemblyCopyright("Copyright © Burgy Benjamin 2015")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
+[assembly: AssemblyTitle("ObjectDisposed.Fody")]
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("175ad7fd-a688-44a0-9645-9e96bec0ce61")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.*")]
---
+++ src/ObjectDisposed.Fody/Properties/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyProduct("ObjectDisposed.Fody")]
+[assembly: AssemblyCopyright("Copyright © Burgy Benjamin 2015")]
+[assembly: AssemblyVersion("0.2.1.*")]
+[assembly: NeutralResourcesLanguage("en")]
+
+#if DEBUG
+[assembly : AssemblyConfiguration("Debug")]
+#else
+
+[assembly: AssemblyConfiguration("Release")]
+#endif
